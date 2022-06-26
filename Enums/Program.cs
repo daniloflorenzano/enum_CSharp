39cac5f127fb9d5331f89c061d25ccae17b8f3a5
Danilo Florenzano <daniloflorenzano1@outlook.com>
2022-06-25 23:46:01 -0300

    first commit

---
+++ Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿using Project.Entities;
+using Project.Entities.Enums;
+using System;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
